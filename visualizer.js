362505b1f724c7d8b03774223bb254cafb8d5bb3
jbeltrami <joaopbeltrami@gmail.com>
2023-03-14 09:50:32 -0300

    Set up HTML canvas
    Create canvas element, make main function run on load.
    Main sets the canvas context and size.

---
+++ visualizer.js
@@ -0,0 +1,7 @@
+function main() {
+  const canvas = document.getElementById("my-canvas");
+  const ctx = canvas.getContext("2d");
+
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
