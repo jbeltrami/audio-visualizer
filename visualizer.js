ec89e64427b3ac5e7186ad69ad584dca06e12dd5
jbeltrami <joaopbeltrami@gmail.com>
2023-03-14 19:12:53 -0300

    Generate colorful visualizer
    Create a createBars function the loops from 1 to 256 (size of dataArray captured by mic).
    Each time the llop goes through generates a new bar.
    The values passed to the bars represent fractions of the canvas.
    Create conditional inside animate function.
    Get samples from microphone and pass them into bar update method.
    Finally, set the height of each bar based on micInput value.

--- visualizer.js
+++ visualizer.js
@@ -15,8 +15,8 @@ function main() {
     }
 
     update(micInput) {
-      // this.height = micInput;
-      this.x++;
+      this.height = micInput * 1000;
+      // this.x++;
     }
 
     // Use context as an argument, to prevent manipulation on the global context
@@ -26,15 +26,35 @@ function main() {
     }
   }
 
-  const bar1 = new Bar(10, 10, 100, 200, "blue");
+  const microphone = new Microphone();
+
+  let bars = [];
+  let barWidth = canvas.width / 256;
+
+  function createBars() {
+    for (let i = 0; i < 256; i++) {
+      const color = `hsl(${i * 2}, 100%, 50%)`;
+      const bar = bars.push(
+        new Bar(i * barWidth, canvas.height / 2, 1, 20, color)
+      );
+    }
+  }
+
+  createBars();
 
   function animate() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    if (microphone.initialized) {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    bar1.draw(ctx);
-    // generate audio samples from mic
-    // update bars
+      const samples = microphone.getSamples();
 
+      // generate audio samples from mic
+      // update bars
+      bars.forEach((bar, i) => {
+        bar.update(samples[i]);
+        bar.draw(ctx);
+      });
+    }
     requestAnimationFrame(animate);
   }
 
