ff00725bccdead0baa8c99f1f4ad9859f386f6f9
jbeltrami <joaopbeltrami@gmail.com>
2023-03-14 10:06:05 -0300

    Create visualizer base functionality.
    Create a class Bar that will serve as a blueprint for bars.
    Add constructor, update and draw methods, to address their behavior.
    Create an animate function and use requestAnimationFrame to run it recursively.

--- visualizer.js
+++ visualizer.js
@@ -4,4 +4,39 @@ function main() {
 
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
+
+  class Bar {
+    constructor(x, y, width, height, color) {
+      this.x = x;
+      this.y = y;
+      this.width = width;
+      this.height = height;
+      this.color = color;
+    }
+
+    update(micInput) {
+      // this.height = micInput;
+      this.x++;
+    }
+
+    // Use context as an argument, to prevent manipulation on the global context
+    draw(context) {
+      context.fillStyle = this.color;
+      context.fillRect(this.x, this.y, this.width, this.height);
+    }
+  }
+
+  const bar1 = new Bar(10, 10, 100, 200, "blue");
+
+  function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    bar1.draw(ctx);
+    // generate audio samples from mic
+    // update bars
+
+    requestAnimationFrame(animate);
+  }
+
+  animate();
 }
