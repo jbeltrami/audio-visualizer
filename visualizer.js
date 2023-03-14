061125f16de399c5bfda29260e4c587328b68084
jbeltrami <joaopbeltrami@gmail.com>
2023-03-14 19:49:34 -0300

    This commit adjusts the visualizer shape.
    Play with different value properties, to draw different shapes in the canvas.

--- visualizer.js
+++ visualizer.js
@@ -6,23 +6,43 @@ function main() {
   canvas.height = window.innerHeight;
 
   class Bar {
-    constructor(x, y, width, height, color) {
+    constructor(x, y, width, height, color, index) {
       this.x = x;
       this.y = y;
       this.width = width;
       this.height = height;
       this.color = color;
+      this.index = index;
     }
 
     update(micInput) {
-      this.height = micInput * 1000;
+      const sound = micInput * 1500;
+
+      if (sound > this.height) {
+        this.height = sound;
+      } else {
+        this.height -= this.height * 0.03;
+      }
       // this.x++;
     }
 
     // Use context as an argument, to prevent manipulation on the global context
-    draw(context) {
-      context.fillStyle = this.color;
-      context.fillRect(this.x, this.y, this.width, this.height);
+    draw(context, vol) {
+      context.strokeStyle = this.color;
+      context.save();
+
+      context.translate(canvas.width / 2, canvas.height / 2);
+      context.rotate(this.index * 0.05);
+
+      // Play with vol variable inside scale
+      context.scale(2, 2);
+
+      context.beginPath();
+      context.moveTo(0, 0);
+      context.lineTo(0, this.height);
+      context.stroke();
+
+      context.restore();
     }
   }
 
@@ -35,7 +55,7 @@ function main() {
     for (let i = 0; i < 256; i++) {
       const color = `hsl(${i * 2}, 100%, 50%)`;
       const bar = bars.push(
-        new Bar(i * barWidth, canvas.height / 2, 1, 20, color)
+        new Bar(i * barWidth, canvas.height / 2, 1, 200, color, i)
       );
     }
   }
@@ -47,12 +67,13 @@ function main() {
       ctx.clearRect(0, 0, canvas.width, canvas.height);
 
       const samples = microphone.getSamples();
+      const volume = microphone.getVolume();
 
       // generate audio samples from mic
       // update bars
       bars.forEach((bar, i) => {
         bar.update(samples[i]);
-        bar.draw(ctx);
+        bar.draw(ctx, volume);
       });
     }
     requestAnimationFrame(animate);
